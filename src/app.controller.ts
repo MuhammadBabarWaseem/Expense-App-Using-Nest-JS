c226df5a064d21f20c43643b19a81f04f3bc77fa
MuhammadBabarWaseem <mbabarwaseem@gmail.com>
2023-02-02 21:35:12 +0500

    Get Dummy Data by report type in postman

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
+import { data, ReportType } from './data';
 
 //:type for income , expense it changes dynamically
+// From 1 hour 8 min Minutes Video https://www.youtube.com/watch?v=BiN-xzNkH_0
+
 @Controller('report/:type')
 export class AppController {
   @Get()
-  getAllReports() {
-    return [];
+  getAllReports(@Param('type') type: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return data.report.filter((report) => report.type === reportType);
   }
 
   @Get(':id')
---
+++ src/data.ts
@@ -0,0 +1,44 @@
+export enum ReportType {
+  INCOME = 'income',
+  EXPENSE = 'expense',
+}
+
+export const data: Data = {
+  report: [
+    {
+      id: 'uuid1',
+      source: 'Salary',
+      amount: 25000,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.INCOME,
+    },
+    {
+      id: 'uuid2',
+      source: 'Tution',
+      amount: 15000,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.INCOME,
+    },
+    {
+      id: 'uuid3',
+      source: 'Food',
+      amount: 800,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.EXPENSE,
+    },
+  ],
+};
+
+interface Data {
+  report: {
+    id: string;
+    source: string;
+    amount: number;
+    created_at: Date;
+    updated_at: Date;
+    type: 'income' | 'expense';
+  }[];
+}
